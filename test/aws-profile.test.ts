96cd6713d88fb0dfd3c51e9827d437ab357c4f2a
Alfredo-del-Rayo <99233341+Alfredo-del-Rayo@users.noreply.github.com>
2025-11-09 16:33:40 -0800

    Initial commit

---
+++ test/aws-profile.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsProfile from '../lib/aws-profile-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-profile-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsProfile.AwsProfileStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
